5e2f657731e8d11d6302ece7afaf2464f4686df1
Muhamad Rama <rama378@gmail.com>
2023-01-05 00:10:05 +0700

    test: add button component test without prop and with clear style prop

--- src/App/tests/common/Button.test.js
+++ src/App/tests/common/Button.test.js
@@ -6,8 +6,20 @@ import Button from '../../common/components/Button/Button';
 const renderer = new ShallowRenderer();
 
 describe('<Button />', () => {
-  it('should render and match the snapshot', () => {
+  it('should render and match the snapshot no prop given', () => {
+    renderer.render(<Button>Button</Button>);
+    const renderedOutput = renderer.getRenderOutput();
+    expect(renderedOutput).toMatchSnapshot();
+  });
+
+  it('should render and match the snapshot given prop style default', () => {
     renderer.render(<Button style="default">Button</Button>);
+    const renderedOutput = renderer.getRenderOutput();
+    expect(renderedOutput).toMatchSnapshot();
+  });
+
+  it('should render and match the snapshot given prop style clear', () => {
+    renderer.render(<Button style="clear">Button</Button>);
     const renderedOutput = renderer.getRenderOutput();
     expect(renderedOutput).toMatchSnapshot();
   });
