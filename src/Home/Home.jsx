bfff5d686d5fc66ad8afd0b60bbd226f25d0fb86
Muhamad Rama <rama378@gmail.com>
2023-01-05 02:18:39 +0700

    feat: implement list component in home page with sample anime list in JSON file

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,13 @@
+import List from './components/List/List';
 import Search from './components/Search/Search';
 import './Home.css';
+import animeList from './anime-list.json';
 
 const Home = () => {
   return (
     <div className='Home'>
       <Search />
+      <List list={animeList} />
     </div>
   );
 }
