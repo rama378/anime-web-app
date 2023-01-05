87a72085392da37c37e49e34112d1e61ed5a01d8
Muhamad Rama <rama378@gmail.com>
2023-01-05 13:07:51 +0700

    refactor: show anime image, title, rating, synopsis, genre, and trailer in detail page with dummy data

--- src/Detail/Detail.jsx
+++ src/Detail/Detail.jsx
@@ -1,12 +1,52 @@
 import { useParams } from 'react-router-dom';
 import './Detail.css';
+import {ReactComponent as UserRatingIcon} from "../App/common/assets/images/icon/star-comment-alt.svg";
+
+// TEST!!!
+import animeList from '../Home/anime-list.json';
+import { formatNumber } from '../App/common/utils/numberUtils';
 
 const Detail = () => {
   let { id } = useParams();
+  
+  // TEST!!
+  const detail = animeList.data[0];
+
+  const getGenres = () => {
+    let genres = [];
+
+    detail?.genres.map((genre) => genres.push(genre.name));
+
+    return genres.join(", ");
+  };
 
   return (
     <div className='Detail'>
-      Detail
+      <div>
+        <img src={detail?.images?.webp?.image_url} className='Detail__image' />
+      </div>
+      <div>
+        <div className='Detail__title'>{detail?.title} ({detail?.year})</div>
+        <div className='Detail__label'>Rating</div>
+        <div>
+          <UserRatingIcon className='Detail__ratingicon' /> {detail?.score} ({formatNumber(detail?.scored_by)})
+        </div>
+
+        <div className='Detail__label'>Synopsis</div>
+        <div>
+          {detail?.synopsis}
+        </div>
+
+        <div className='Detail__label'>Genre</div>
+        <div>
+          {getGenres()}
+        </div>
+
+        <div className='Detail__label'>Watch Trailer</div>
+        <div>
+          <iframe width="350" height="200" src={detail?.trailer?.embed_url}></iframe> 
+        </div>
+      </div>
     </div>
   );
 }
